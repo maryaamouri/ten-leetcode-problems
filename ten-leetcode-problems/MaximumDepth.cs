3b086fce7446363c2396a915d9266aad9413919d
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 16:10:50 +0300

    fix bug, make methods static

--- ten-leetcode-problems/MaximumDepth.cs
+++ ten-leetcode-problems/MaximumDepth.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace ten_leetcode_problems
 {
-    internal class MaximumDepth
+    internal  class MaximumDepth
     {
        
-            public int Get(TreeNode root)
-            {
-                return dfs(root);
-            }
-            private int dfs(TreeNode root, int depth = 0)
+            public static int  Get(TreeNode root, int depth = 0)
             {
                 if (root == null) return 0;
-                return max(dfs(root.left, depth) + 1, dfs(root.right, depth) + 1);
+                return max(Get(root.left, depth) + 1, Get(root.right, depth) + 1);
             }
-            private int max(int x, int y) { if (x > y) return x; else return y; }
-
+            private static int max(int x, int y) {
+            if (x > y) return x; else return y;
         }
+        static internal TreeNode CreateSampleTreeNode()
+        {
+            TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
+
+            return root;
+        }
+
+
+    }
     }
 
--- ten-leetcode-problems/Program.cs
+++ ten-leetcode-problems/Program.cs
@@ -8,6 +8,8 @@ internal class Program
         //Question #8
         Console.WriteLine("----------------");
         Console.WriteLine("Qestion#8: Maximum Depth of Binary Tree");
-        
+        TreeNode root = MaximumDepth.CreateSampleTreeNode();
+        int maxDepth = MaximumDepth.Get(root);
+        Console.WriteLine($"max depth = {maxDepth}");
     }
 }
