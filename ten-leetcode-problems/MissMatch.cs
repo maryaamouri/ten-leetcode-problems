cf9d9b96edfcc8a4552f7497f17fabddacb89cbf
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 16:14:30 +0300

    add missmatch class

---
+++ ten-leetcode-problems/MissMatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ten_leetcode_problems
+{
+    internal static class MissMatch
+    {
+        internal static int[] Find(int[] nums)
+        {
+            int[] result = new int[2];
+            Array.Sort(nums);
+            int sumi = 0;
+            int sumj = 0;
+            for (int i = 0, j = 1; i < nums.Length; i++, j++)
+            {
+
+
+                // find repeated number
+                if (i + 1 < nums.Length && nums[i] == nums[i + 1])
+                {
+                    result[0] = nums[i];
+                    nums[i] = 0;
+                }
+
+                sumi += nums[i];
+                sumj += j;
+            }
+            // find loss number
+            result[1] = sumj - sumi;
+            return result.ToArray();
+        }
+    }
+}
