c493295de1b2bbac40e36c934fc30a0d2a20b8ab
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 15:24:38 +0300

    add q1, contains doublicated, test class in main

---
+++ ten-leetcode-problems/ContainsDoublicated.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ten_leetcode_problems
+{
+    static internal class ContainsDoublicated
+    {
+        public static bool GetContainsNearbyDuplicate(int[] nums, int k)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+                for (int j = i + 1; j < nums.Length & j <= i + k; j++)
+                {
+                    if (nums[i] == nums[j])
+                        return true;
+
+                }
+            return false;
+        }
+    }
+}
--- ten-leetcode-problems/Program.cs
+++ ten-leetcode-problems/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using static ten_leetcode_problems.ContainsDoublicated;
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, C#!");
+
+        // Q1
+
+        Console.WriteLine("Question #1:");
+        int[] nums = { 1, 2, 3, 1 };
+        int k = 3;
+        bool containsDoublicated = GetContainsNearbyDuplicate(nums, k);
+        Console.WriteLine($"Do the array  {string.Join(" , ", nums)} contains doublicated nymbers on k = {k} near indeces?");
+        Console.WriteLine(containsDoublicated);
+
+
     }
 }
