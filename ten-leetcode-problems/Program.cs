1562f7e04d3b019ff9e6d4c733d34992bb69be04
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 15:32:03 +0300

    test q3 in main

--- ten-leetcode-problems/Program.cs
+++ ten-leetcode-problems/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using static ten_leetcode_problems.LongestPalendrom;
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, C#!");
+        // Q3
+        string s = "abccccdd";
+        Console.WriteLine("Question #3:");
+        int longestPalendrome = GetLongestPalindrome(s);
+        Console.WriteLine($"The Longest Palendron String that can be generated from {s} is {longestPalendrome}");
+
     }
 }
