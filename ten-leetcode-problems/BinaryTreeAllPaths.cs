6ee3048c12c88f1c1e47f92f08cd471c66f0fe43
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 15:40:37 +0300

    add treenode, helper class

---
+++ ten-leetcode-problems/BinaryTreeAllPaths.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ten_leetcode_problems.TreeNode;
+namespace ten_leetcode_problems
+{
+    internal static class BinaryTreeAllPaths
+    {
+
+    }
+}
