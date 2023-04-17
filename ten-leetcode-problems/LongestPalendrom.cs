5db5d30c6f379ff4ad0dd873d1a9b5e22890fe10
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 15:30:52 +0300

    add q3, longest palendrom

---
+++ ten-leetcode-problems/LongestPalendrom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ten_leetcode_problems
+{
+    internal static class LongestPalendrom
+    {
+        public static int GetLongestPalindrome(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (char ch in s)
+            {
+                if (!dic.ContainsKey(ch))
+                    dic.Add(ch, 1);
+                else
+                    dic[ch]++;
+            }
+            bool containsOddFreq = false;
+            int longestPalendrom = 0;
+            foreach (var key in dic.Keys)
+            {
+                if (dic[key] % 2 == 1)
+                {
+                    containsOddFreq = true;
+                    dic[key]--;
+                }
+                longestPalendrom += dic[key];
+
+
+            }
+            if (containsOddFreq)
+                longestPalendrom++;
+            return longestPalendrom;
+
+        }
+    }
+}
