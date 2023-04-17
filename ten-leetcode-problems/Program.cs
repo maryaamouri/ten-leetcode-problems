4213caed85dc59dfb4758642a351ddc89007be16
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-17 15:34:58 +0300

    configure main for q4

--- ten-leetcode-problems/Program.cs
+++ ten-leetcode-problems/Program.cs
@@ -3,5 +3,11 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, C#!");
+
+        // Q4
+        Console.WriteLine("Question #4:");
+        string parantheses = "()[]{}";
+        bool isValidParantheses = IsValidParentheses(parantheses);
+        Console.WriteLine($"The string {parantheses} considered as {isValidParantheses} valid parantheses");
     }
 }
