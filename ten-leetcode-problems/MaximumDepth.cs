919ac51532eb8c014e3a57b7b61539c5533604db
Marya_Amouri <marya.ammori@gmail.com>
2023-05-09 10:08:53 +0300

    replace data types with var

--- ten-leetcode-problems/MaximumDepth.cs
+++ ten-leetcode-problems/MaximumDepth.cs
@@ -19,7 +19,7 @@ namespace ten_leetcode_problems
         }
         static internal TreeNode CreateSampleTreeNode()
         {
-            TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
+            var root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
 
             return root;
         }
--- ten-leetcode-problems/Program.cs
+++ ten-leetcode-problems/Program.cs
@@ -8,8 +8,8 @@ internal class Program
         //Question #8
         Console.WriteLine("----------------");
         Console.WriteLine("Qestion#8: Maximum Depth of Binary Tree");
-        TreeNode root = MaximumDepth.CreateSampleTreeNode();
-        int maxDepth = MaximumDepth.Get(root);
+        var root = MaximumDepth.CreateSampleTreeNode();
+        var maxDepth = MaximumDepth.Get(root);
         Console.WriteLine($"max depth = {maxDepth}");
     }
 }
