c8f8bedf67444432921b5321871dfd2e43b5281b
Marya_Amouri <marya.ammori@gmail.com>
2023-05-09 10:10:17 +0300

    use Math.Max instead of user defined math method

--- ten-leetcode-problems/MaximumDepth.cs
+++ ten-leetcode-problems/MaximumDepth.cs
@@ -8,15 +8,11 @@ namespace ten_leetcode_problems
 {
     internal  class MaximumDepth
     {
-       
-            public static int  Get(TreeNode root, int depth = 0)
+        public static int  Get(TreeNode root, int depth = 0)
             {
                 if (root == null) return 0;
-                return max(Get(root.left, depth) + 1, Get(root.right, depth) + 1);
+                return Math.Max(Get(root.left, depth) + 1, Get(root.right, depth) + 1);
             }
-            private static int max(int x, int y) {
-            if (x > y) return x; else return y;
-        }
         static internal TreeNode CreateSampleTreeNode()
         {
             var root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
